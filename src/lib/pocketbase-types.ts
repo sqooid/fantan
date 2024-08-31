ad79cd47f13f3fcdb024722b836e6900c5622efe
sqooid <sqooiddev@gmail.com>
2024-08-31 17:31:36 +1000

    create novel and chapter entities

--- src/lib/pocketbase-types.ts
+++ src/lib/pocketbase-types.ts
@@ -6,6 +6,8 @@ import type PocketBase from 'pocketbase'
 import type { RecordService } from 'pocketbase'
 
 export enum Collections {
+	Chapters = "chapters",
+	Novels = "novels",
 	Users = "users",
 }
 
@@ -33,21 +35,42 @@ export type AuthSystemFields<T = never> = {
 
 // Record types for each collection
 
+export type ChaptersRecord = {
+	content?: string
+	novel: RecordIdString
+	published?: boolean
+	title?: string
+	value: string
+}
+
+export type NovelsRecord = {
+	description?: string
+	editors?: RecordIdString[]
+	owner: RecordIdString
+	title: string
+}
+
 export type UsersRecord = {
 	avatar?: string
 	name?: string
 }
 
 // Response types include system fields and match responses from the PocketBase API
+export type ChaptersResponse<Texpand = unknown> = Required<ChaptersRecord> & BaseSystemFields<Texpand>
+export type NovelsResponse<Texpand = unknown> = Required<NovelsRecord> & BaseSystemFields<Texpand>
 export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export type CollectionRecords = {
+	chapters: ChaptersRecord
+	novels: NovelsRecord
 	users: UsersRecord
 }
 
 export type CollectionResponses = {
+	chapters: ChaptersResponse
+	novels: NovelsResponse
 	users: UsersResponse
 }
 
@@ -55,5 +78,7 @@ export type CollectionResponses = {
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
 export type TypedPocketBase = PocketBase & {
+	collection(idOrName: 'chapters'): RecordService<ChaptersResponse>
+	collection(idOrName: 'novels'): RecordService<NovelsResponse>
 	collection(idOrName: 'users'): RecordService<UsersResponse>
 }
