f0f374eb0e307ee2ccd53ab99488cd76890ef61e
sqooid <sqooiddev@gmail.com>
2024-09-02 12:40:20 +1000

    fix browser import

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 	import EmailRegister from '$lib/components/email-register.svelte';
 	import OauthSigninButton from '$lib/components/oauth-signin-button.svelte';
 	import GithubIcon from '$lib/icons/github-icon.svelte';
 	import GoogleIcon from '$lib/icons/google-icon.svelte';
 	import { authStore } from '$lib/stores/pocketbase';
-	import { browser } from 'process';
 
 	if (browser && $authStore?.isValid) {
 		goto('/');
