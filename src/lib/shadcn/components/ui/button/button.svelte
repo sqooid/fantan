2cdf86b5da6da659de881e642625d7fc7ddd5109
sqooid <sqooiddev@gmail.com>
2024-09-02 18:11:21 +1000

    customise enter button with autoenter click

--- src/lib/shadcn/components/ui/button/button.svelte
+++ src/lib/shadcn/components/ui/button/button.svelte
@@ -2,6 +2,7 @@
 	import { Button as ButtonPrimitive } from 'bits-ui';
 	import { type Events, type Props, buttonVariants } from './index.js';
 	import { cn } from '$lib/shadcn/utils.js';
+	import { createEventDispatcher } from 'svelte';
 
 	type $$Props = Props;
 	type $$Events = Events;
@@ -11,7 +12,19 @@
 	export let size: $$Props['size'] = 'default';
 	export let builders: $$Props['builders'] = [];
 	export { className as class };
+	export let autoEnter = false;
+
+	const dispatch = createEventDispatcher();
+	const onKeyPress = (e: KeyboardEvent) => {
+		if (autoEnter && e.key === 'Enter') {
+			e.preventDefault();
+			//@ts-ignore
+			dispatch('click');
+		}
+	};
 </script>
+
+<svelte:document on:keypress={onKeyPress} />
 
 <ButtonPrimitive.Root
 	{builders}
