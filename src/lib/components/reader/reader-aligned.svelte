bb2cb2a89538c23ceb67a18b49cddfbe0755c8b5
sqooid <sqooiddev@gmail.com>
2024-09-08 21:57:44 +1000

    create aligned reader

---
+++ src/lib/components/reader/reader-aligned.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let sourceContent: string;
+	export let translatedContent: string;
+
+	const dispatch = createEventDispatcher<{ openNote: string }>();
+</script>
+
+<style lang="postcss">
+</style>
