0df3e611452a8568147faf41e9881592177be69d
sqooid <sqooiddev@gmail.com>
2024-09-16 00:18:11 +1000

    clean up unused import

--- src/routes/read/[novelSlug]/+page.svelte
+++ src/routes/read/[novelSlug]/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import GlobalLoadingBar from '$lib/components/global-loading-bar.svelte';
 	import NovelsPage from '$lib/pages/novels-page.svelte';
 	import type { NovelsResponse } from '$lib/pocketbase-types';
 	import { pb } from '$lib/stores/pocketbase';
-	import { markdownText } from '$lib/utils/data-transform';
 	import { useQuery } from '@sveltestack/svelte-query';
-	import type { PageData } from './$types';
 
 	$: novelSlug = $page.params.novelSlug;
-
-	export let data: PageData;
 
 	const novelQuery = useQuery<NovelsResponse>({ enabled: false });
 	$: if (novelSlug) {
