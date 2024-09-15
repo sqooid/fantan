7f72cabee9cc36e2063d315593f4bf75264a5935
sqooid <sqooiddev@gmail.com>
2024-09-16 00:12:02 +1000

    add sitemap generator

--- src/routes/sitemap/+server.ts
+++ src/routes/sitemap/+server.ts
@@ -1,7 +1,35 @@
+import { pb } from '$lib/stores/pocketbase';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async (request) => {
-	// TODO: generate actual sitemap urls
+	const origin = request.url.origin;
+	console.log(origin);
+
+	const novels = await pb
+		.collection('novels')
+		.getFullList({ filter: pb.filter('chaptersCount > 0'), fields: 'slug,id,updated' });
+	const novelsMap = novels.reduce(
+		(acc, novel) => {
+			acc[novel.id] = novel.slug;
+			return acc;
+		},
+		{} as Record<string, string>
+	);
+	const chapters = await pb
+		.collection('chapters')
+		.getFullList({ filter: pb.filter('published = true'), fields: 'novel,volume,value,updated' });
+	const urls = novels
+		.map(
+			(novel) =>
+				`<url><loc>${origin}/read/${novel.slug}</loc><lastmod>${new Date(novel.updated).toISOString()}</lastmod></url>`
+		)
+
+		.concat(
+			chapters.map((chapter) => {
+				const novelSlug = novelsMap[chapter.novel];
+				return `<url><loc>${origin}/read/${novelSlug}/volume-${chapter.volume}/chapter-${chapter.value}</loc><lastmod>${new Date(chapter.updated).toISOString()}</lastmod></url>`;
+			})
+		);
 	return new Response(
 		`
 		<?xml version="1.0" encoding="UTF-8" ?>
@@ -13,7 +41,7 @@ export const GET: RequestHandler = async (request) => {
 			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
 			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
 		>
-			<!-- <url> elements go here -->
+		${urls.join('\n')}
 		</urlset>`.trim(),
 		{
 			headers: {
