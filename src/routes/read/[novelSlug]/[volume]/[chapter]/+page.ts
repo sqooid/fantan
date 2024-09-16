7504d9e9a0087ae70ab1e49475b65da0000ee949
sqooid <sqooiddev@gmail.com>
2024-09-16 11:09:29 +1000

    debug logging

--- src/routes/read/[novelSlug]/[volume]/[chapter]/+page.ts
+++ src/routes/read/[novelSlug]/[volume]/[chapter]/+page.ts
@@ -4,26 +4,32 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params, parent }) => {
 	if (!browser) {
-		let data = await parent();
+		let { novelId } = await parent();
 		const chapterPattern = /^chapter-(\d+)$/;
 		const volumePattern = /^volume-(\d+)$/;
 		const volume = params.volume.match(volumePattern)?.[1];
 		const chapter = params.chapter.match(chapterPattern)?.[1];
+		console.log({ novelId, volume, chapter });
+
 		try {
 			const result = await pb.collection('chapters').getFirstListItem(
 				pb.filter('novel = {:novelId} && volume = {:volume} && value = {:chapter}', {
-					novelId: data.novelId,
+					novelId: novelId,
 					volume,
 					chapter
 				}),
 				{ fields: 'id,value,volume' }
 			);
+			console.log(result);
+
 			return {
 				chapterId: result.id,
 				volume: result.volume,
 				chapter: result.value
 			};
 		} catch (error) {
+			console.log(error);
+
 			return {};
 		}
 	}
